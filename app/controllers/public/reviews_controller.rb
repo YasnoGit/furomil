506603f1f1ad8973f72842533edcdfa4ca1aa803
EC2 Default User <132417648+YasnoGit@users.noreply.github.com>
2023-07-18 11:39:00 +0000

    +レビュー機能非同期通信完了

--- app/controllers/public/reviews_controller.rb
+++ app/controllers/public/reviews_controller.rb
@@ -1,16 +1,19 @@
 class Public::ReviewsController < ApplicationController
 
+  # 下記遷移設定非同期通信用コメントアウト
+
   def create
-    facility = Facility.find(params[:facility_id])
+    @facility = Facility.find(params[:facility_id])
     review = current_customer.reviews.new(review_params)
-    review.facility_id = facility.id
+    review.facility_id = @facility.id
     review.save
-    redirect_to facility_path(facility)
+    # redirect_to facility_path(facility)
   end
 
   def destroy
+    @facility = Facility.find(params[:facility_id])
     Review.find(params[:id]).destroy
-    redirect_to facility_path(params[:facility_id])
+    # redirect_to facility_path(params[:facility_id])
   end
 
   private
