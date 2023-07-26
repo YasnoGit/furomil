60e683a75668598d138fb1b5086c860f716fc8d9
EC2 Default User <132417648+YasnoGit@users.noreply.github.com>
2023-07-26 09:02:29 +0000

    レイアウト終了、残りシード設定、

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,44 @@ Admin.create!(
   email: 'admin@aaa',
   password: 'adminadmin'
 )
+
+# 会員情報
+customers = Customer.create!(
+  [
+    {email: 'olivia@example.com', last_name: '', first_name: '', last_name_kana: '', first_name_kana: '', post_code: '', address: '', phone_number: '', is_deleted: '', created_at: '', updated_at: '', password: 'password' },
+    {email: 'james@example.com', name: 'James', password: 'password', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user2.jpg"), filename:"sample-user2.jpg")},
+    {email: 'lucas@example.com', name: 'Lucas', password: 'password', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user3.jpg"), filename:"sample-user3.jpg")}
+  ]
+)
+
+# 施設情報
+Facility.create!(
+  [
+    {area_id: '', name: '', introduction: '', price: '', open: '', holiday: '', score: '', address: '',, latitude: '', longitude: '', created_at: '', updated_at: '' },
+    {shop_name: 'Cavello', image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post1.jpg"), filename:"sample-post1.jpg"), caption: '大人気のカフェです。', user_id: users[0].id },
+    {shop_name: '和食屋せん', image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post2.jpg"), filename:"sample-post2.jpg"), caption: '日本料理は美しい！', user_id: users[1].id },
+    {shop_name: 'ShoreditchBar', image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post3.jpg"), filename:"sample-post3.jpg"), caption: 'メキシコ料理好きな方にオススメ！', user_id: users[2].id }
+  ]
+)
+
+# エリア情報
+Area.create!(
+  [
+    {name: '', created_at: '', updated_at: '' },
+  ]
+)
+
+# レビュー情報
+Review.create!(
+  [
+    {customer_id: '', facility_id: '', comment: '', score: '', created_at: '', updated_at: '' },
+  ]
+)
+
+# ブックマーク情報
+Bookmark.create!(
+  [
+    {customer_id: '', facility_id: '', created_at: '', updated_at: '' },
+  ]
+)
+
